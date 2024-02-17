ddce9b7cae885dac3e2db1efc2d6ac225388ecfe
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-02-17 10:56:25 -0600

    0.1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿Console.WriteLine("Ingrese Nombre");
+string nombre = Console.ReadLine().ToUpper();
+Console.WriteLine("Ingrese Pais");
+string pais = Console.ReadLine();
 
-namespace ALGO
-{
 
-}
 
